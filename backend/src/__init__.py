06ed2c8c13ac69eebd8a5d2c7253e0f8e63aa012
yds05238 <jy392@cornell.edu>
2024-08-31 11:15:09 -0400

    Updated users to handle jwt token authentication

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -2,12 +2,16 @@ import os
 
 from flask import Flask
 from flask_admin import Admin
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.middleware.proxy_fix import ProxyFix
 
 # instantiate the extensions
 db = SQLAlchemy()
 admin = Admin(template_mode="bootstrap3")
+cors = CORS()
+bcrypt = Bcrypt()
 
 
 def create_app(script_info=None):
@@ -24,6 +28,8 @@ def create_app(script_info=None):
 
     # set up extensions
     db.init_app(app)
+    # cors.init_app(app, resources={r"*": {"origins": "*"}})
+    bcrypt.init_app(app)
     if os.getenv("FLASK_ENV") == "development":
         admin.init_app(app)
 
